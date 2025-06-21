c312dce58fdea336d1a51be2b98647d14092f022
Roya1998 <royayusifbayli@gmail.com>
2025-06-21 12:27:42 +0000

    new c file created

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+include <stdio>
+
+
+int main (void)
+{
+
+	int n;
+	if (n>0)
+		printf("%d is positive\n", n)
+	
+	else if (n==0)
+		printf ("%d is zero\n" , n)
+
+	else (n<0) 
+	printf ("%d is negative\n" ,n )
+
+	return (0)	
+}
+
