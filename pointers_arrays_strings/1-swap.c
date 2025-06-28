42bcc8c8a9be3bd542767a865c36323dab2c8ee6
Roya1998 <royayusifbayli@gmail.com>
2025-06-28 17:09:41 +0000

    swap task update

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,21 +1,17 @@
 /*
- * File: 2-strlen.c
+ * File: 1-swap.c
  */
 
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @str: The string to get the length of.
- *
- * Return: The length of @str.
+ * swap_int - Swaps the values of two integers.
+ * @a: The first integer to be swapped.
+ * @b: The second integer to be swapped.
  */
-size_t _strlen(const char *str)
+void swap_int(int *a, int *b)
 {
-	size_t length = 0;
-
-	while (*str++)
-		length++;
-
-	return (length);
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
