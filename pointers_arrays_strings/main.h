45d8b6d015c9077e42a1b698b756e1b721e4a098
Roya1998 <royayusifbayli@gmail.com>
2025-06-29 12:14:12 +0000

    0th task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -32,7 +32,6 @@ int _atoi(char *s);
 
 /**
  * File: holberton.h
- * Auth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x05-pointers_arrays_strings directory.
  */
@@ -50,5 +49,30 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif
+
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
