770b34cbbaaf463d36991f37383b9281ffdcf21d
Roya1998 <royayusifbayli@gmail.com>
2025-07-05 14:06:50 +0000

    0th file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File 0th file 
+ */
+
+/**
+ * struct dog - A dog's basic information
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Defines a struct for storing dog info.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* MAIN_H */
