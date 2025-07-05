86112886c04863c961eba0d5f25b8e7425f4dfea
Roya1998 <royayusifbayli@gmail.com>
2025-07-05 11:35:12 +0000

    betty style change

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,24 +1,24 @@
 /*
- * File: 2nd file 
+ * File: 2-strlen_recursion.c
  */
 
 #include "main.h"
 
 /**
- * strlen_recursion - Returns the length of a string.
+ * _strlen_recursion - Returns the length of a string.
  * @s: The string to be measured.
  *
  * Return: The length of the string.
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+    int len = 0;
 
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
+    if (*s)
+    {
+        len++;
+        len += _strlen_recursion(s + 1);
+    }
 
-	return (len);
+    return (len);
 }
