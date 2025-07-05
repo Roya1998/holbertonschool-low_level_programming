b916dfc8b4cd8221b88d9c87368b604385846781
Roya1998 <royayusifbayli@gmail.com>
2025-07-05 11:30:09 +0000

    betty code add

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -5,7 +5,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of a string.
+ * strlen_recursion - Returns the length of a string.
  * @s: The string to be measured.
  *
  * Return: The length of the string.
