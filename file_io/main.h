8e2f785073a8eb6f0ca5e5bbf964812df3d31c3b
Roya1998 <royayusifbayli@gmail.com>
2025-07-28 15:35:04 +0000

    main file change

--- file_io/main.h
+++ file_io/main.h
@@ -1,22 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
+#include <unistd.h>
+#include <fcntl.h>
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(int ac, char **av)
-{
-	int res;
+int create_file(const char *filename, char *text_content);
+int _putchar(char c);
 
-	if (ac != 3)
-	{
-		dprintf(2, "Usage: %s filename text\n", av[0]);
-		exit(1);
-	}
-	res = create_file(av[1], av[2]);
-	printf("-> %i)\n", res);
-	return (0);
-}
+#endif
