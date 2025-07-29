24c189353f9ab35266e8aea3168458d73826031a
Roya1998 <royayusifbayli@gmail.com>
2025-07-29 11:20:20 +0000

    0th task correct

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,37 +1,51 @@
 #include "main.h"
 
 /**
- * read_textfile - read text and print it to POSIX
- * @filename: name of file char
- * @letters:number of letters to read size_t
- * Return: read the file display it or error 0
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: the actual number of letters it could read and print,
+ *         or 0 if the file cannot be opened or read, or if filename is NULL,
+ *         or if write fails or does not write the expected amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, rd, wd;
-	char *buff = malloc(sizeof(char) * letters);
+	int fd;
+	ssize_t rd, wd;
+	char *buff;
 
 	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
+		free(buff);
 		return (0);
 	}
 
-	fd = open(filename, O_RDWR);
-	if (fd == -1)
-	{
-		return (0);
-	}
 	rd = read(fd, buff, letters);
 	if (rd == -1)
 	{
+		free(buff);
+		close(fd);
 		return (0);
 	}
+
 	wd = write(STDOUT_FILENO, buff, rd);
-	if (wd == -1)
+	if (wd == -1 || wd != rd)
 	{
+		free(buff);
+		close(fd);
 		return (0);
 	}
-	close(fd);
+
 	free(buff);
+	close(fd);
 	return (wd);
 }
